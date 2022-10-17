a76c76685ce381d95bbad5607c6be030bf266b99
dyahbalzammo <dyahxad.s+sbalzammo@gmail.com>
2022-10-17 06:07:45 +0000

    up

--- server/connect.js
+++ server/connect.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql')
 
+// clever-cloud.com
+// fahmidaud354@gmail.com
 const conn = mysql.createConnection({
     host: 'baf1jbsistamhsc1hrt3-mysql.services.clever-cloud.com',
     user: 'uzmhgdvp2mkoygxe',
