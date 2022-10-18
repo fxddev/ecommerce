3d44d9190b43e84bd868dfe7cecaecf36189ad24
dyahbalzammo <dyahxad.s+sbalzammo@gmail.com>
2022-10-18 10:43:34 +0000

    up

--- client/src/routes/Produk.svelte
+++ client/src/routes/Produk.svelte
@@ -12,7 +12,7 @@
         navigate(`/login?seller`, { replace: true });
     } else {
         cred = JSON.parse(get_cred);
-        const data = cred.data
+        const data = cred.data;
         const role_id = data.role_id;
         console.log(role_id);
 
@@ -30,7 +30,7 @@
 
         var config = {
             method: "post",
-            url: `${api_url}/product`, 
+            url: `${api_url}/product`,
             headers: {
                 "Content-Type": "application/json",
             },
@@ -62,5 +62,79 @@
 {#if product.length === 0}
     Tidak ada produk
 {:else}
-    produknya
+    <div class="overflow-x-auto w-full">
+        <table class="table w-full">
+            <!-- head -->
+            <thead>
+                <tr>
+                    <th>
+                        <label>
+                            <input type="checkbox" class="checkbox" />
+                        </label>
+                    </th>
+                    <th>Name</th>
+                    <th>Harga</th>
+                    <th>Stok</th>
+                    <th>Aktif</th>
+                    <th />
+                </tr>
+            </thead>
+            <tbody>
+                {#each product as p}
+                    <tr>
+                        <th>
+                            <label>
+                                <input type="checkbox" class="checkbox" />
+                            </label>
+                        </th>
+                        <td>
+                            <div class="flex items-center space-x-3">
+                                <div class="avatar">
+                                    <div class="mask mask-squircle w-12 h-12">
+                                        <img
+                                            src="https://placeimg.com/80/80/people"
+                                            alt="Avatar Tailwind CSS Component"
+                                        />
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="font-bold">{p.nama}</div>
+                                    <div class="text-sm opacity-50">SKU:-</div>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            {p.harga}
+                            <br />
+                            <span class="badge badge-ghost badge-sm"
+                                >Desktop Support Technician</span
+                            >
+                        </td>
+                        <td>{p.stok}</td>
+                        <td>
+                            {#if p.status === "active"}
+                                <input type="checkbox" class="toggle" checked />
+                            {:else}
+                                <input type="checkbox" class="toggle" />
+                            {/if}
+                        </td>
+                        <th>
+                            <button class="btn btn-ghost btn-xs">details</button
+                            >
+                        </th>
+                    </tr>
+                {/each}
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th />
+                    <th>Name</th>
+                    <th>Harga</th>
+                    <th>Stok</th>
+                    <th>Aktif</th>
+                    <th />
+                </tr>
+            </tfoot>
+        </table>
+    </div>
 {/if}
