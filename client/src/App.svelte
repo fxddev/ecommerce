1873b181e77a130b28953dfa1fe6b71b296aa45d
dyahbalzammo <dyahxad.s+sbalzammo@gmail.com>
2022-10-28 00:05:03 +0000

    up

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   // import outes from "./components/penjual/Drawer.svelte";
 
-  localStorage.setItem("api_url", "https://8003-fxddev-ecommerce-e8pgi2obh1x.ws-us72.gitpod.io");
+  localStorage.setItem("api_url", "https://8003-fxddev-ecommerce-e8pgi2obh1x.ws-us73.gitpod.io");
 
   import Routes from "./Routes.svelte";
 </script>
