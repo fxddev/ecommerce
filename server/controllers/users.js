cbc5e3ce23a80dcaf57c94d8885426970122890f
fxddev <fxddev@gmail.com>
2022-10-29 02:36:06 +0000

    up

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -58,7 +58,69 @@ exports.create = async (req, res) => {
     // }
     const id_product = req.body.id_product;
 
+    const role_id = req.body.role_id;
+
+    let table = ""
+    if (parseInt(role_id) === 2) {
+        table = "penjual"
+    } else if (parseInt(role_id) === 3) {
+        table = "pembeli"
+    }
+
     conn.query(`INSERT INTO rating (id_product, id_pembeli, ulasan, star, created_at, update_at) VALUES(${id_product}, ${id_pembeli}, "${ulasan}", ${star}, "${created_at}", "${update_at}")`, async function (error, rows, fields) {
+
+        if (error) {
+            res.status(200).send({
+                message: "Error",
+                data: error
+            });
+
+        } else {
+
+            res.status(200).send({
+                message: "Sukses"
+            });
+
+        }
+    })
+};
+
+exports.create = async (req, res) => {
+
+    // {
+    //     "id_product": 1,
+    //     "id_pembeli": 1,
+    //     "ulasan": "Mantap",
+    //     "star": 4,
+    //     "created_at": "26473463",
+    //     "update_at": "26473463"
+    // }
+    const id_product = req.body.id_product;
+
+    const role_id = req.body.role_id;
+
+    let table = ""
+    if (parseInt(role_id) === 2) {
+        table = "penjual"
+    } else if (parseInt(role_id) === 3) {
+        table = "pembeli"
+    }
+
+    conn.query(`UPDATE ${table} SET "nama" =
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     'edit', `created_at` = '1665560538620', `update_at` = '1665560538620' WHERE `pembeli`.`id` = 2 INSERT INTO rating (id_product, id_pembeli, ulasan, star, created_at, update_at) VALUES(${id_product}, ${id_pembeli}, "${ulasan}", ${star}, "${created_at}", "${update_at}")`, async function (error, rows, fields) {
 
         if (error) {
             res.status(200).send({
