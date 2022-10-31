f1f4c8d870d5a00ac5c013d4cd7c4fd0466ebf4a
dyahbalzammo <dyahxad.s+sbalzammo@gmail.com>
2022-10-31 00:46:35 +0000

    up

--- client/src/routes/Profile.svelte
+++ client/src/routes/Profile.svelte
@@ -267,14 +267,6 @@
                 class="input w-full max-w-xs"
                 bind:value={origins}
             />
-            <select class="select w-full max-w-xs">
-                <option disabled selected>Pick your favorite Simpson</option>
-                <option>Homer</option>
-                <option>Marge</option>
-                <option>Bart</option>
-                <option>Lisa</option>
-                <option>Maggie</option>
-            </select>
             <span>Alamat Lengkap</span>
             <input
                 type="text"
