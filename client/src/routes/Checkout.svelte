486afdfe280caf91d750cfbd06ea7becd7cae812
fxddev <fxddev@gmail.com>
2022-11-02 01:01:10 +0000

    up

--- client/src/routes/Checkout.svelte
+++ client/src/routes/Checkout.svelte
@@ -1,10 +1,22 @@
 <script>
     import { cartSelected } from "../store/MyWritableStore";
 
+    let carts = [];
     cartSelected.subscribe((value) => {
-        console.log("value");
-        console.log(value);
+        if (parseInt(value) != 0) {
+            carts = value;
+            console.log("carts");
+            console.log(carts);
+        }
     });
+
+    async function getCarts() {
+        console.log("Sedang get carts");
+    }
+
+    if (carts.length === 0) {
+        getCarts();
+    }
 </script>
 
 ini hal chekout
