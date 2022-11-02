c98b6a073365ec1467cd5b29eab9ab47b8b8f227
fxddev <fxddev@gmail.com>
2022-11-02 01:08:43 +0000

    up

--- client/src/routes/Checkout.svelte
+++ client/src/routes/Checkout.svelte
@@ -1,5 +1,13 @@
 <script>
+    import axios from "axios";
+    import { navigate } from "svelte-navigator";
+
     import { cartSelected } from "../store/MyWritableStore";
+
+    const api_url = localStorage.getItem("api_url");
+
+    const get_cred = localStorage.getItem("cred");
+    console.log(get_cred);
 
     let carts = [];
     cartSelected.subscribe((value) => {
@@ -11,7 +19,38 @@
     });
 
     async function getCarts() {
-        console.log("Sedang get carts");
+        if (get_cred === null) {
+            navigate(`/login?customer`, { replace: true });
+        } else {
+            const cred = JSON.parse(get_cred);
+            const data = cred.data;
+            const id_pembeli = data.id;
+            console.log("id_pembeli");
+            console.log(id_pembeli);
+
+            const obj = {
+                id_pembeli: parseInt(id_pembeli),
+            };
+            var payload = JSON.stringify(obj);
+
+            var config = {
+                method: "post",
+                url: `${api_url}/keranjang`,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                data: payload,
+            };
+            try {
+                const resp = await axios(config);
+                const data = await resp.data;
+                console.log(data);
+
+                carts = data.data;
+            } catch (error) {
+                console.error(`Axios error..: ${error}`);
+            }
+        }
     }
 
     if (carts.length === 0) {
@@ -19,4 +58,29 @@
     }
 </script>
 
-ini hal chekout
+<div>
+    <div>
+        {#each carts as c}
+            <div class="card card-side bg-base-100 shadow-xl">
+                <figure>
+                    <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">{c.nama_product}</h2>
+                    <p>{c.jumlah} barang</p>
+                    <p>Rp. {c.harga}</p>
+                    <div class="card-actions justify-end">
+                        <select class="select w-full max-w-xs">
+                            <option disabled selected>Pengiriman</option>
+                            <option>Homer</option>
+                            <option>Marge</option>
+                            <option>Bart</option>
+                            <option>Lisa</option>
+                            <option>Maggie</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        {/each}
+    </div>
+</div>
