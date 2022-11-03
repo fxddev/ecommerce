d2a5f7ab3eaa18ad449709d91b6ce6e7b712c73f
fxddev <fxddev@gmail.com>
2022-11-03 23:27:19 +0000

    up

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   // import outes from "./components/penjual/Drawer.svelte";
 
-  localStorage.setItem("api_url", "https://8003-fxddev-ecommerce-jp73trz5bhi.ws-us73.gitpod.io");
+  localStorage.setItem("api_url", "https://8003-fxddev-ecommerce-qed88kkzyro.ws-us74.gitpod.io");
   
   import Routes from "./Routes.svelte";
 </script>
